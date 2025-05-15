5940e14b54aed18b713ee15ce3116089d86ca258
eaglevisionsolutions <info@eaglevisionsolutions.ca>
2025-05-15 12:29:29 -0400

    add support to show alternate page offline, split index to include template files

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var offlineResponse = "<h2>Welcome To Houseparty</h2>" + "<p>There Seems to be a problem with your connection.</p>" + "<p>We look forward to serving you when you are online again.</p>";
 
 self.addEventListener("fetch", function(event){
+    fetch
     event.respondWith(
         fetch(event.request).catch(function(){
-            return new Response(
-                offlineResponse,
-                {headers: {"Content-Type" : "text/html"}}
-            );
+            return fetch("/index-offline.html");
         })
     );
 });
---
+++ index-offline.html
@@ -0,0 +1,4 @@
+<include src="includes/header.html"></include>
+  <!-- Add your site or application offline content here -->
+  <p>Hello world! You are Offline right now.</p>
+<include src="includes/footer.html"></include>
