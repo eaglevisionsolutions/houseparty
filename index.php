624ef27cad9cfc3b78ad94988ded126ced82ae0a
eaglevisionsolutions <87085280+eaglevisionsolutions@users.noreply.github.com>
2025-05-15 17:23:02 -0400

    Delete index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-<include src="includes/header.html"></include>
-  <!-- Add your site or application content here -->
-  <p>Hello world! This is House Party.</p>
-<include src="includes/footer.html"></include>
-  
