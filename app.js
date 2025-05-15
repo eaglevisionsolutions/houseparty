60ff1c34fba2c374310d87088669e3b4583cc7eb
eaglevisionsolutions <info@eaglevisionsolutions.ca>
2025-05-15 11:37:24 -0400

    update file to fix function error

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 self.addEventListener("fetch", function(event){
-    event.respondWith(fetch(event.request).catch(function{
+    event.respondWith(fetch(event.request).catch(function(){
         return new Response(
             "<h2>Welcome To Houseparty</h2>" + "<p>There Seems to be a problem with your connection.</p>" + "<p>We look forward to serving you when you are online again.</p>",{
                 headers: {
