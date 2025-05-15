8267934371aca55d70c24f8fdff729b22f6b2037
eaglevisionsolutions <info@eaglevisionsolutions.ca>
2025-05-15 16:34:39 -0400

    build init for loading jquery and intializing the app

--- local/js/init.js
+++ local/js/init.js
@@ -5,3 +5,49 @@ if("serviceWorker" in navigator){
         console.log("Service worker registration Failed:", err);
     });
 }
+var requiredFiles = [
+    '/local/js/app.js',
+];
+
+function loadScript(url, callback, async) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.onload = function() {
+      callback();
+    };
+    script.onerror = function() {
+      console.error('Failed to load script: ' + url);
+      if (url.startsWith('https://')) {
+        loadScript(url.replace('https://', 'http://'), callback);
+      } else if (!url.startsWith('file://')) {
+        loadScript('file://' + url, callback);
+      } else {
+        console.error('Failed to load local fallback.');
+      }
+    };
+    script.src = url;
+    if(async === true){
+        document.body.appendChild(script);
+    }else{
+        document.head.appendChild(script);
+    }
+    
+  }
+
+
+document.onload = function(){
+    // load jquery from google hosted libary if can't get fallback to local version
+    loadScript("https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js", function(){
+        if(window.Jquery){
+            initializeApp();
+        }else{
+            loadScript("vendor/jquery/jquery-3.7.1.js", function(){
+                if(window.jQuery){
+                    initialzeApp();
+                }else{
+                    console.log("Unable to Load Jquery");
+                }
+            });
+        }
+    });
+}
